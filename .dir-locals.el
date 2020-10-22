6336e5287a72b80a993eab21ac4b62bbc273c7ee
Joel Grunbaum <joelgrun@gmail.com>
2020-10-22 13:30:32 +1100

    removed accidentally committed file

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-((nil .
-	  ((eval .
-			 (let ((root (projectile-project-root)))
-			   (setq-local company-clang-arguments
-						   (list (concat "-I" root "include")))
-			   (setq-local flycheck-clang-include-path
-						   (list (concat root "include"))))))))
